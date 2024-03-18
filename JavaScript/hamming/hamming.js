77d714e233621e23abfa0c543124c2dd0e8be966
mantis <morauszki.andras@gmail.com>
2024-03-18 22:22:53 +0100

    Hamming distance solution in JS

--- JavaScript/hamming/hamming.js
+++ JavaScript/hamming/hamming.js
@@ -1,8 +1,10 @@
-//
-// This is only a SKELETON file for the 'Hamming' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const compute = (strandA, strandB) => {
+  if (strandA.length !== strandB.length)
+    throw new Error('strands must be of equal length');
 
-export const compute = () => {
-  throw new Error('Remove this statement and implement this function');
+  let difference = 0;
+  for (let i = 0; i < strandA.length; i++) {
+    if (strandA.slice(i, i + 1) !== strandB.slice(i, i + 1)) difference++;
+  }
+  return difference;
 };

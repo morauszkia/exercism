f15aa356466e4c31e02d7e8e87ee6e9ed1c4902b
mantis <morauszki.andras@gmail.com>
2024-04-09 15:49:06 +0200

    Sieve solution in JS

--- JavaScript/sieve/sieve.js
+++ JavaScript/sieve/sieve.js
@@ -3,6 +3,22 @@
 // convenience to get you started writing code faster.
 //
 
-export const primes = () => {
-  throw new Error('Remove this statement and implement this function');
+export const primes = (limit) => {
+  if (limit < 2) return [];
+
+  const isPrime = new Array(limit + 1).fill(true);
+  isPrime.splice(0, 2, false, false);
+  let primesList = [];
+
+  for (let i = 2; i <= limit; i++) {
+    if (isPrime[i]) {
+      primesList.push(i);
+      for (let j = i ** 2; j <= limit; j += i) {
+        isPrime[j] = false;
+      }
+    }
+  }
+
+  console.log(isPrime);
+  return primesList;
 };

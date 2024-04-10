f3d2cf51e05a291158ab27bfab7a03b4a7a23e65
mantis <morauszki.andras@gmail.com>
2024-04-10 22:26:54 +0200

    Sieve solutions in JavaScript & TypeScript

--- JavaScript/sieve/sieve.js
+++ JavaScript/sieve/sieve.js
@@ -8,17 +8,17 @@ export const primes = (limit) => {
 
   const isPrime = new Array(limit + 1).fill(true);
   isPrime.splice(0, 2, false, false);
-  let primesList = [];
 
-  for (let i = 2; i <= limit; i++) {
+  for (let i = 2; i <= Math.sqrt(limit); i++) {
     if (isPrime[i]) {
-      primesList.push(i);
       for (let j = i ** 2; j <= limit; j += i) {
         isPrime[j] = false;
       }
     }
   }
 
-  console.log(isPrime);
-  return primesList;
+  return isPrime.reduce(
+    (primesList, current, idx) => (current ? [...primesList, idx] : primesList),
+    []
+  );
 };
--- TypeScript/sieve/sieve.ts
+++ TypeScript/sieve/sieve.ts
@@ -1,3 +1,13 @@
-export function primes() {
-  throw new Error('Remove this statement and implement this function')
+export function primes(limit: number): number[] {
+  if (limit < 2) return [];
+
+  let potentialPrimes = Array.from({ length: limit - 1 }, (_, i) => i + 2);
+
+  for (let i = 2; i <= Math.sqrt(limit); i++) {
+    potentialPrimes = potentialPrimes.filter(
+      (num) => num === i || num % i !== 0
+    );
+  }
+
+  return potentialPrimes;
 }

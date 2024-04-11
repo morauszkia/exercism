f6117ce1de1b0cc79498f000a91a591fcfc5c542
mantis <morauszki.andras@gmail.com>
2024-04-11 06:34:36 +0200

    Sieve solution in Python

--- Python/sieve/sieve.py
+++ Python/sieve/sieve.py
@@ -1,2 +1,13 @@
+from math import sqrt, ceil
+
+
 def primes(limit):
-    pass
+    potential = [i for i in range(2, limit + 1)]
+
+    for i in range(2, ceil(sqrt(limit))):
+        potential = list(filter(lambda x: x == i or x % i != 0, potential))
+
+    print(potential)
+
+
+primes(10)

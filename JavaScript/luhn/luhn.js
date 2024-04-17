95d03e1006a185563fb08255c6e5bc78ecf0ee75
mantis <morauszki.andras@gmail.com>
2024-04-17 22:14:27 +0200

    Imperative solution for Luhn using for loop in JavaScript

--- JavaScript/luhn/luhn.js
+++ JavaScript/luhn/luhn.js
@@ -1,8 +1,23 @@
-//
-// This is only a SKELETON file for the 'Luhn' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const valid = (number) => {
+  let sum = 0;
+  let odd = true;
+  let digitCount = 0;
+  for (let i = number.length - 1; i >= 0; i--) {
+    const currentChar = number[i];
+    if (currentChar === ' ') continue;
+    if (+currentChar === NaN) return false;
 
-export const valid = () => {
-  throw new Error('Remove this statement and implement this function');
+    sum += odd
+      ? +currentChar
+      : +currentChar < 5
+      ? +currentChar * 2
+      : +currentChar * 2 - 9;
+
+    odd = !odd;
+    digitCount++;
+  }
+
+  if (digitCount < 2) return false;
+
+  return sum % 10 === 0;
 };

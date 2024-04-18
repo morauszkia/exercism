4dc164a9c3569314b60f0635c0a9230aad9a3ebf
mantis <morauszki.andras@gmail.com>
2024-04-18 16:11:35 +0200

    Scrabble Score solutions in Python and R

--- Python/scrabble-score/scrabble_score.py
+++ Python/scrabble-score/scrabble_score.py
@@ -1,2 +1,6 @@
 def score(word):
-    pass
+    LETTERS = list("AEIOULNRSTDGBCMPFHVWYKJXQZ")
+    VALUES = [int(digit) + 1 for digit in "00000000001122223333347799"]
+    LETTER_SCORES = {k: v for k, v in zip(LETTERS, VALUES)}
+
+    return sum([LETTER_SCORES[char] for char in word.upper()])

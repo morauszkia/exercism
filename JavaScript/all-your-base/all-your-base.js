68d04fe02274b99380b155a30e24086da5467542
mantis <morauszki.andras@gmail.com>
2024-05-08 19:12:25 +0200

    All Your Base solution in TS & refactored solution in JS

--- JavaScript/all-your-base/all-your-base.js
+++ JavaScript/all-your-base/all-your-base.js
@@ -1,6 +1,22 @@
 export const convert = (digits, inputBase, outputBase) => {
+  checkBase(inputBase, outputBase);
+  checkDigits(digits, inputBase);
+
+  let num = digits
+    .map((digit, i) => digit * inputBase ** (digits.length - i - 1))
+    .reduce((sum, next) => sum + next, 0);
+  let result = [];
+  do {
+    result = [num % outputBase, ...result];
+    num = Math.floor(num / outputBase);
+  } while (num > 0);
+  return result;
+};
+const checkBase = (inputBase, outputBase) => {
   if (inputBase < 2) throw new Error("Wrong input base");
   if (outputBase < 2) throw new Error("Wrong output base");
+};
+const checkDigits = (digits, inputBase) => {
   if (
     digits.length == 0 ||
     (digits.length > 1 && digits[0] === 0) ||
@@ -8,18 +24,4 @@ export const convert = (digits, inputBase, outputBase) => {
     digits.some((d) => d >= inputBase)
   )
     throw new Error("Input has wrong format");
-
-  let num = digits
-    .map((digit, i) => digit * inputBase ** (digits.length - i - 1))
-    .reduce((sum, next) => sum + next, 0);
-
-  // if (num == 0) return digits;
-
-  let result = [];
-  do {
-    result = [num % outputBase, ...result];
-    num = Math.floor(num / outputBase);
-  } while (num > 0);
-
-  return result;
 };

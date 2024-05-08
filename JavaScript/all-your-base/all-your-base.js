780ff948ee85adcdbf7d8a1e33b4a08a98ff589e
mantis <morauszki.andras@gmail.com>
2024-05-08 18:51:41 +0200

    All Your Base solution in JS

--- JavaScript/all-your-base/all-your-base.js
+++ JavaScript/all-your-base/all-your-base.js
@@ -1,8 +1,25 @@
-//
-// This is only a SKELETON file for the 'All Your Base' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const convert = (digits, inputBase, outputBase) => {
+  if (inputBase < 2) throw new Error("Wrong input base");
+  if (outputBase < 2) throw new Error("Wrong output base");
+  if (
+    digits.length == 0 ||
+    (digits.length > 1 && digits[0] === 0) ||
+    digits.some((d) => d < 0) ||
+    digits.some((d) => d >= inputBase)
+  )
+    throw new Error("Input has wrong format");
 
-export const convert = () => {
-  throw new Error('Remove this statement and implement this function');
+  let num = digits
+    .map((digit, i) => digit * inputBase ** (digits.length - i - 1))
+    .reduce((sum, next) => sum + next, 0);
+
+  // if (num == 0) return digits;
+
+  let result = [];
+  do {
+    result = [num % outputBase, ...result];
+    num = Math.floor(num / outputBase);
+  } while (num > 0);
+
+  return result;
 };

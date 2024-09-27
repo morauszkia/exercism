bb83c6b671fe06a036fe3138fe49c680b00bfe11
mantis <morauszki.andras@gmail.com>
2024-09-27 19:20:29 +0200

    Isogram solutions in JS

--- JavaScript/isogram/isogram.js
+++ JavaScript/isogram/isogram.js
@@ -1,8 +1,5 @@
-//
-// This is only a SKELETON file for the 'Isogram' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-export const isIsogram = () => {
-  throw new Error('Remove this statement and implement this function');
+export const isIsogram = (word) => {
+  // Uses capture group to check if any [a-z] character
+  // is repeated after 0 or more characters.
+  return !word.match(/([a-z]).*\1/i);
 };

27e93d78a275ff39c64b83985e066567e6d56297
mantis <morauszki.andras@gmail.com>
2024-12-07 23:12:38 +0100

    Bank Account Solution in Python

--- Python/bank-account/bank_account.py
+++ Python/bank-account/bank_account.py
@@ -1,18 +1,45 @@
+import threading
+
+
 class BankAccount:
     def __init__(self):
-        pass
+        self.is_open = False
+        self.lock = threading.Lock()
+
+    def check_open_status(self, expected):
+        if expected == "open" and not self.is_open:
+            raise ValueError("account not open")
+        if expected == "closed" and self.is_open:
+            raise ValueError("account already open")
 
     def get_balance(self):
-        pass
+        with self.lock:
+            self.check_open_status("open")
+            return self.balance
 
     def open(self):
-        pass
+        with self.lock:
+            self.check_open_status("closed")
+            self.is_open = True
+            self.balance = 0
 
     def deposit(self, amount):
-        pass
+        if amount < 0:
+            raise ValueError("amount must be greater than 0")
+        with self.lock:
+            self.check_open_status("open")
+            self.balance += amount
 
     def withdraw(self, amount):
-        pass
+        if amount < 0:
+            raise ValueError("amount must be greater than 0")
+        with self.lock:
+            self.check_open_status("open")
+            if amount > self.balance:
+                raise ValueError("amount must be less than balance")
+            self.balance -= amount
 
     def close(self):
-        pass
+        with self.lock:
+            self.check_open_status("open")
+            self.is_open = False

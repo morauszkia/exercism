7d0d039427060d3bfe1f43bdbb483222a1ea3d8f
mantis <morauszki.andras@gmail.com>
2024-12-07 18:33:15 +0100

    Bank Account Solution in JS

--- JavaScript/bank-account/bank-account.js
+++ JavaScript/bank-account/bank-account.js
@@ -1,36 +1,53 @@
-//
-// This is only a SKELETON file for the 'Bank Account' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export class BankAccount {
   constructor() {
-    throw new Error('Remove this statement and implement this function');
+    this._open = false;
+    this._balance = 0;
   }
 
   open() {
-    throw new Error('Remove this statement and implement this function');
+    if (!this._open) {
+      this._open = true;
+      this._balance = 0;
+    } else {
+      throw new ValueError();
+    }
   }
 
   close() {
-    throw new Error('Remove this statement and implement this function');
+    if (this._open) {
+      this._open = false;
+    } else {
+      throw new ValueError();
+    }
   }
 
-  deposit() {
-    throw new Error('Remove this statement and implement this function');
+  deposit(amount) {
+    if (this._open && amount >= 0) {
+      this._balance += amount;
+    } else {
+      throw new ValueError();
+    }
   }
 
-  withdraw() {
-    throw new Error('Remove this statement and implement this function');
+  withdraw(amount) {
+    if (this._open && amount <= this._balance && amount >= 0) {
+      this._balance -= amount;
+    } else {
+      throw new ValueError();
+    }
   }
 
   get balance() {
-    throw new Error('Remove this statement and implement this function');
+    if (this._open) {
+      return this._balance;
+    } else {
+      throw new ValueError();
+    }
   }
 }
 
 export class ValueError extends Error {
   constructor() {
-    super('Bank account error');
+    super("Bank account error");
   }
 }
